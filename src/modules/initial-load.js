f084bd68d5882cc29c0d5db1c5f6ba3560907e33
Andrew Bonas <aabonas@hotmail.com>
2021-01-24 13:28:37 -0800

    footer added

--- src/modules/initial-load.js
+++ src/modules/initial-load.js
@@ -4,8 +4,8 @@ function createrHeader(id, text) {
   const name = document.createElement('h1');
   name.textContent = text;
   const btnCtn = document.createElement('div');
-  const homeBtn = createBtn('home','home');
-  const menuBtn = createBtn('menu', 'menu');
+  const homeBtn = createBtn('home','Home');
+  const menuBtn = createBtn('menu', 'Menu');
   header.appendChild(name);
   header.appendChild(btnCtn);
   btnCtn.appendChild(homeBtn);
@@ -25,19 +25,34 @@ function createBtn(id, text) {
 function createMain(id) {
   const main = document.createElement('main');
   main.setAttribute('id', id);
-  const aboutBtn = createBtn('about', 'about');
-  main.appendChild(aboutBtn);
+  const div = document.createElement('div')
+
+  const aboutBtn = createBtn('about', 'Find Out More');
+  main.appendChild(div);
+  div.appendChild(aboutBtn);
   return main
+}
+
+function createFooter(id, text) {
+  const footer = document.createElement('footer');
+  footer.setAttribute('id',id);
+  const brand = document.createElement('h1');
+  brand.textContent = text;
+  footer.appendChild(brand);
+  return footer;
 }
 
 function loadPage() {
   const content = document.getElementById('content');
 
-  const header = createrHeader('header', 'The best');
+  const header = createrHeader('header', 'Lettuce B. Frank');
   content.appendChild(header);
 
   const hero = createMain('hero');
-  content.appendChild(hero)
+  content.appendChild(hero);
+
+  const footer = createFooter('footer', 'The Best Rest');
+  content.appendChild(footer);
 }
 
 export default loadPage;
