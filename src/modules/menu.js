5901f70edc0d2c8ccf2bd624c49d2ee1fc6d8884
Andrew Bonas <aabonas@hotmail.com>
2021-01-26 15:13:57 -0800

    jpg src url changed for gh pages

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -12,7 +12,7 @@ function menuPage() {
   <div class="row">
     <div class="col-lg-6 mb-4 d-flex justify-content-center">
       <div class="card" style="width: 30rem">
-        <img class="card-img-top" src="/assets/sourdough.jpg" alt="bread">
+        <img class="card-img-top" src="./assets/sourdough.jpg" alt="bread">
         <div class="card-body">
           <h5 class="card-title">Sourdough Bread</h5>
           <p class="card-text">Authentic fresh sourdough bread. Small batch.
@@ -23,31 +23,7 @@ function menuPage() {
     </div>
     <div class="col-lg-6 mb-4 d-flex justify-content-center">
       <div class="card" style="width: 30rem">
-        <img class="card-img-top" src="/assets/sourdough.jpg" alt="bread">
-        <div class="card-body">
-          <h5 class="card-title">Sourdough Bread</h5>
-          <p class="card-text">Authentic fresh sourdough bread. Small batch.
-            Fermentation process and baking all done in house in rustic cast iron dutch oven.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-lg-6 mb-4 d-flex justify-content-center">
-      <div class="card" style="width: 30rem">
-        <img class="card-img-top" src="/assets/sourdough.jpg" alt="bread">
-        <div class="card-body">
-          <h5 class="card-title">Sourdough Bread</h5>
-          <p class="card-text">Authentic fresh sourdough bread. Small batch.
-            Fermentation process and baking all done in house in rustic cast iron dutch oven.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 mb-4 d-flex justify-content-center">
-      <div class="card" style="width: 30rem">
-        <img class="card-img-top" src="/assets/sourdough.jpg" alt="bread">
+        <img class="card-img-top" src="./assets/sourdough.jpg" alt="bread">
         <div class="card-body">
           <h5 class="card-title">Sourdough Bread</h5>
           <p class="card-text">Authentic fresh sourdough bread. Small batch.
@@ -60,7 +36,7 @@ function menuPage() {
   <div class="row">
     <div class="col-lg-6 mb-4 d-flex justify-content-center">
       <div class="card" style="width: 30rem">
-        <img class="card-img-top" src="/assets/sourdough.jpg" alt="bread">
+        <img class="card-img-top" src="./assets/sourdough.jpg" alt="bread">
         <div class="card-body">
           <h5 class="card-title">Sourdough Bread</h5>
           <p class="card-text">Authentic fresh sourdough bread. Small batch.
@@ -71,7 +47,31 @@ function menuPage() {
     </div>
     <div class="col-lg-6 mb-4 d-flex justify-content-center">
       <div class="card" style="width: 30rem">
-        <img class="card-img-top" src="/assets/sourdough.jpg" alt="bread">
+        <img class="card-img-top" src="./assets/sourdough.jpg" alt="bread">
+        <div class="card-body">
+          <h5 class="card-title">Sourdough Bread</h5>
+          <p class="card-text">Authentic fresh sourdough bread. Small batch.
+            Fermentation process and baking all done in house in rustic cast iron dutch oven.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-6 mb-4 d-flex justify-content-center">
+      <div class="card" style="width: 30rem">
+        <img class="card-img-top" src="./assets/sourdough.jpg" alt="bread">
+        <div class="card-body">
+          <h5 class="card-title">Sourdough Bread</h5>
+          <p class="card-text">Authentic fresh sourdough bread. Small batch.
+            Fermentation process and baking all done in house in rustic cast iron dutch oven.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-6 mb-4 d-flex justify-content-center">
+      <div class="card" style="width: 30rem">
+        <img class="card-img-top" src="./assets/sourdough.jpg" alt="bread">
         <div class="card-body">
           <h5 class="card-title">Sourdough Bread</h5>
           <p class="card-text">Authentic fresh sourdough bread. Small batch.
